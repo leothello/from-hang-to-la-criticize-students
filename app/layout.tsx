a0da01860b995346159ff9f362081592066197f5
Leothello Gracive <1573848781@qq.com>
2026-08-03 00:44:43 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,16 @@
-出口 Const 元数据={
-  ={标题:'从夯到拉锐评25中所有学生',
+export const metadata = {
+  title: '从夯到拉锐评25中所有学生',
 }
 
-出口 默认 功能 RootLayout({ children }  ) {
+export default function RootLayout({ children }) {
   return (
-    <超文本标记语言朗="zh-CN">
+    <html lang="zh-CN">
       <head>
-        <<<<<<<<<<脚本src="https://cdn.tailwindcss.com"></script >
+        <script src="https://cdn.tailwindcss.com"></script>
       </head>
-      <身体风格={{ backgroundColor: '#efe8dc' }} className="min-h-screen">
+      <body style={{ backgroundColor: '#efe8dc' }} className="min-h-screen">
         {children}
-      </body >
-    </html >
+      </body>
+    </html>
   )
 }
